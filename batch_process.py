335776552b2fa4b5fb8cd7b63ca5541c47701f60
DavidSalas540 <David.salasmepersonal@gmail.com>
2025-11-23 16:35:42 +0100

    First commit

---
+++ batch_process.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from video_process import pipeline
+from config import (
+    FLAT_PIN_POINT_FOLDER, FLAT_PLATFORM_FOLDER,
+    KICK_PIN_POINT_FOLDER, KICK_PLATFORM_FOLDER,
+    SLICE_PIN_POINT_FOLDER, SLICE_PLATFORM_FOLDER,
+    OUTPUT_VIDEOS_FOLDER, OUTPUT_CSVS_FOLDER
+)
+
+def ensure_output_dirs():
+    OUTPUT_VIDEOS_FOLDER.mkdir(parents=True, exist_ok=True)
+    OUTPUT_CSVS_FOLDER.mkdir(parents=True, exist_ok=True)
+
+
+def process_folder(folder: Path, serve_effect: str, stance: str):
+    if not folder.exists():
+        print(f"Folder not found: {folder}")
+        return
+        
+    print(f"Processing folder: {folder.name}")
+
+    videos = sorted(folder.glob("*.mp4"))
+    if not videos:
+        print(f"No .mp4 videos found in folder: {folder.name}")
+        return
+
+    for video_path in videos:
+        analyzed_name = f"{serve_effect}_{stance}_{video_path.name}"
+        out_video = OUTPUT_VIDEOS_FOLDER / analyzed_name
+        
+        csv_name = f"{serve_effect}_{stance}_{video_path.stem}_landmarks.csv"
+        csv_out = OUTPUT_CSVS_FOLDER / csv_name 
+        
+        pipeline(video_path, out_video, csv_out)
+
+
+if __name__ == "__main__":
+    ensure_output_dirs()
+    process_folder(FLAT_PIN_POINT_FOLDER, "flat", "pinpoint")
+    process_folder(FLAT_PLATFORM_FOLDER, "flat", "platform")
+    process_folder(KICK_PIN_POINT_FOLDER, "kick","pinpoint")
+    process_folder(KICK_PLATFORM_FOLDER, "kick","platform")
+    process_folder(SLICE_PIN_POINT_FOLDER, "slice","pinpoint")
+    process_folder(SLICE_PLATFORM_FOLDER, "slice","platform")
+    
---
+++ single_video.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from video_process import *
+
+BASE_FOLDER = Path(r"C:\Users\david\Videos\pruebasTFG")
+INPUT_VIDEO = BASE_FOLDER / "102.mp4"
+OUTPUT_FOLDER = BASE_FOLDER / "Results"
+
+
+def main(serve_effect: str, stance: str):
+    
+    OUTPUT_FOLDER.mkdir(parents=True, exist_ok=True)
+    
+    analyzed_name = f"{serve_effect}_{stance}_{INPUT_VIDEO.name}"
+    out_video = OUTPUT_FOLDER / analyzed_name
+        
+    csv_name = f"{serve_effect}_{stance}_{INPUT_VIDEO.stem}_landmarks.csv"
+    csv_out = OUTPUT_FOLDER / csv_name 
+    
+    print(f"--- Procesando {INPUT_VIDEO.name} ---")
+    
+    print("1. Extrayendo...")
+    df_raw, f, w, h = process_video(INPUT_VIDEO)
+    if df_raw is not None:
+        print("2. Limpiando...")
+        df_clean = cleaning_data(df_raw)
+        print("3. Creando video...")
+        create_comparision_video(INPUT_VIDEO, df_raw, f, w, h, df_clean, out_video)
+        print("4. Guardando CSV...")
+        save_csv(df_clean, csv_out)
+        print("¡Prueba finalizada!")
+    else:
+       print("Error: No se pudo procesar el video.")
+
+if __name__ == "__main__":
+    
+    main("kick", "pinpoint")
+   
